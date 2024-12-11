f3d5842b038f2a8de0ea8b1d5ca89f755bbf3378
Amandickz <amandickz@gmail.com>
2024-12-11 14:39:48 -0300

    Testes com a Classe LocalDateTime

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.time.LocalDateTime;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        LocalDateTime DATE_TIME = LocalDateTime.now();
+        System.out.println(DATE_TIME);
+        System.out.println(DATE_TIME.getDayOfMonth());
+        System.out.println(DATE_TIME.getMonthValue());
+        System.out.println(DATE_TIME.getYear());
+
+
 
     }
 }
